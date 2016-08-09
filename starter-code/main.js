e3a06a398b6374efaa7282b8fdfa4152cf8a2543
David Useche <dju079@gmail.com>
2016-08-09 01:24:00 -0400

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ var card1 = "queen";
 var card2 = "queen";
 var card3 = "king";
 var card4= "king";
+
+if(card1===card2){
+
+	alert("You found a match!");
+}
+
+else {
+    alert("Sorry try again");
+
+}
